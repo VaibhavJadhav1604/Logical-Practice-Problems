069f747c823c20c33cd1614a9ed6076db6580ace
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-08 20:40:32 +0530

    Fibonacci Number Till Given Number
    Program To Fibonacci Number Till Given Number

---
+++ FB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class FB
+    {
+        static void Main(string[] args)
+        {
+            int number, a = 0, b = 1, c, i;
+
+            Console.Write("Enter The End Point Till You Want Fibonacci Sequence:");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Fibonacci Series:"+ a +" "+ b);
+
+            for (i = 2; i < number; i++)
+            {
+                c = a + b;
+                Console.Write(" "+ c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
