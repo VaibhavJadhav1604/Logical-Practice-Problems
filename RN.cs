6f9e936c4138afba591f5e1cc6191c9d2d918534
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-08 20:45:20 +0530

    Reverse A Number
    Program To Reverse A Number

---
+++ RN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    internal class RN
+    {
+        static void Main(string[] args)
+        {
+            int n, reverse = 0, rem;
+            Console.Write("Enter Number:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("Reversed Number:" + reverse);
+        }
+    }
+}
