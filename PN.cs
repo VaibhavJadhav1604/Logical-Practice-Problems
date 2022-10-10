66147fb2a7f15c613a41e55eedc282c4f9f951f2
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-10 19:59:45 +0530

    Perfect Number Or Not
    Program To Check Whether Given Number Is Perfect Or Not

---
+++ PN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    internal class PN
+    {
+        static int sum;
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1;  
+            while (i <= n / 2)
+            {
+                
+                if (n % i == 0)
+                { 
+                    sum += i;
+                } 
+                i++;
+            }
+            if (sum == n)
+            {
+                Console.WriteLine(n + ":Is Perfect Number.");
+            }
+            else
+            {  
+                Console.WriteLine(n + ":Is Not Perfect Number.");
+            }        
+        }
+    }
+}
